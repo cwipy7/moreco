72569416809a4099dd083a22ea973c7665de2826
ms621y <ms621y@att.com>
2020-04-06 16:21:32 -0500

    RS: finished python-d3 handshake, info transfer, started d3 barchart

--- visual/server.py
+++ visual/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, send_file
+from flask import Flask, render_template, jsonify, send_file, request
 import pandas as pd
 import json
 
@@ -15,6 +15,35 @@ def load_tags():
 @app.route('/healthcheck')
 def hello_world():
     return 'Server Running!'
+
+# Passing selected tag data back to python server so we can run cosine similarity or other algorithms in python
+@app.route('/tagselection', methods=['GET', 'POST'])           
+def tag_selection_from_d3():
+    if request.method == 'POST':
+        tag_data = request.get_json()
+        print("Server received: ", tag_data)
+        top_movies = cosine_handoff(tag_data)
+        return jsonify(status="success", data=top_movies)
+
+    else:
+        return "Not Implemented"
+        # return jsonify(status="success", data=tag_data)
+
+
+def cosine_handoff(tag_data):
+    # something something cosine
+    movie_names = ['movie1_' + tag_data[0]['tag'], 'movie2_' + tag_data[0]['tag']]
+    top_movies = [
+        {   'movie': movie_names[0],
+            'tag_relevance': '0.99'
+        },
+        {   'movie': movie_names[1],
+            'tag_relevance': '0.70'
+        }
+    ]
+
+    return top_movies
+
 
 
 if __name__ == "__main__":
--- visual/static/js/moreco.js
+++ visual/static/js/moreco.js
@@ -2,6 +2,10 @@ chosen_tags = []
 
 // console.log(window.location.pathname)
 
+var margin = {top: 50, right: 100, bottom: 20, left: 100}
+    , width = (window.innerWidth - margin.left - margin.right) / 2
+    , height = (window.innerHeight - margin.top - margin.bottom) / 2;
+
 d3.dsv(",", "/genome-tags").then(function(data) {
     director_array = []
     
@@ -16,15 +20,86 @@ d3.dsv(",", "/genome-tags").then(function(data) {
         "dataArray": director_array,
         "itemName": "tag",
         "callable": function (items) {
-            //console.dir(items)
+            // console.dir(items)
             chosen_tags = items
         }
     };
 
     $("#transfer1").transfer(settings);
+
+    var svg_barchart = d3.select("#barchart")
+        .append("svg")
+        .attr("width", 700)
+        .attr("height", 400)
+        .append("g")
+        .attr('class', 'barchart')
+        // .style("visibility", "hidden");
+    
+    var x_bar = d3.scaleLinear()
+        .range([0, width])
+        .domain([0, 1]);
+    
+    var y_bar = d3.scaleBand()
+        .range([height, 0], .1)
+        .domain([0, 1]);
+
+    var yAxis_bar = d3.axisLeft(y_bar)
+        .tickSize(0)
+    
+    var xAxis_bar = d3.axisBottom()
+        .scale(x_bar)
+
+    svg_barchart.append("g")
+        .attr("class", "y_axis")
+        .call(yAxis_bar)
+
+    svg_barchart.append("g")
+        .attr("class", "x_axis")
+        .call(xAxis_bar)
+
+    var bars_bar = svg_barchart.selectAll(".bar")
+        .data([])
+        .enter()
+        .append("g")
+
+    bars_bar.append("rect")
+        .attr("class", "bar")
+        .attr("y", function (d) {
+            return y_bar(d.key);
+        })
+        .attr("height", y_bar.bandwidth())
+        .attr("x", 0)
+        .attr("width", function (d) {
+            return x_bar(d.value);
+        })
+        .style("fill", "blue");
+            
+    bars_bar.append("text")
+        .attr("x", width/2)
+        .attr("y", height/2-200)
+        .style("text-anchor", "middle")
+        .attr("dy", ".35em")
     
 });
 
-function search_movie() {
-    console.log(chosen_tags)
+function send_tags_to_server() {
+    // console.log(chosen_tags)
+    $.ajax({
+        type: "POST",
+        url: "/tagselection",
+        contentType: "application/json",
+        data: JSON.stringify(chosen_tags),
+        dataType: "json",
+        success: function(response) {
+            console.log(response);
+            display_top_movies_barchart(response.data);
+        },
+        error: function(err) {
+            console.log(err);
+        }
+    });
+}
+
+function display_top_movies_barchart(top_movies) {
+    console.log("Displaying: " + top_movies)
 }
