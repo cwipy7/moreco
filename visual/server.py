07deca85231a12cbbf32383e312d693a8bcbb2ad
ms621y <ms621y@att.com>
2020-04-07 10:10:48 -0500

    RS: added working barchart, mouse hover interaction

--- visual/server.py
+++ visual/server.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, jsonify, send_file, request
+from flask import Flask, render_template, jsonify, send_file, request, redirect
 import pandas as pd
 import json
+import numpy as np
+import sqlite3
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 app = Flask(__name__)
 
@@ -22,8 +26,8 @@ def tag_selection_from_d3():
     if request.method == 'POST':
         tag_data = request.get_json()
         print("Server received: ", tag_data)
-        top_movies = cosine_handoff(tag_data)
-        return jsonify(status="success", data=top_movies)
+        top_selection = cosine_handoff(tag_data)
+        return jsonify(status="success", data=top_selection)
 
     else:
         return "Not Implemented"
@@ -31,18 +35,61 @@ def tag_selection_from_d3():
 
 
 def cosine_handoff(tag_data):
-    # something something cosine
-    movie_names = ['movie1_' + tag_data[0]['tag'], 'movie2_' + tag_data[0]['tag']]
-    top_movies = [
-        {   'movie': movie_names[0],
-            'tag_relevance': '0.99'
-        },
-        {   'movie': movie_names[1],
-            'tag_relevance': '0.70'
-        }
-    ]
+    # cosine similarity
+    # tags = [1, 3, 8, 10]
+    # top_n, tag_ids = get_top_cosine_similar(tags, top_n=5)
+    # print(top_n)
+    # print(tag_ids)
 
-    return top_movies
+    selected_tag_ids = [tag['value'] for tag in tag_data]
+    top_n, _ = get_top_cosine_similar(selected_tag_ids, top_n=10)
+    top_selection = []
+
+    for id, relevance_score in top_n:
+        top_selection.append({
+            'id': id,
+            'relevance_score': relevance_score
+        })
+
+    print(top_selection)
+    return top_selection
+
+
+def get_conn():
+    db_name = './db/movie_sqlite.db'
+    conn = sqlite3.connect(db_name)
+    return conn
+
+def get_top_cosine_similar(tag_ids, entity_type=['movies','directors'][0], top_n=10):
+    '''
+    tag_ids: list of tag ids to consider
+    
+    return:
+        list of tuples [(entity_id, cosine similarity value), ...],
+        list of tag ids
+    '''
+    prefix = 'tt' if entity_type == 'movies' else 'nn'
+    select_cols = ',\n'.join([f'sum(case when tag_id = {tg} then relevance end) tag_id_{str(tg)}' for tg in tag_ids])
+    sql = f"""
+        select fk_id,
+            {select_cols}
+        from tag_relevance
+        where tag_id in {tuple(tag_ids)}
+        and fk_id like '{prefix}%'
+        group by fk_id;
+    """
+    conn = get_conn()
+    df = pd.read_sql(sql, conn).set_index('fk_id')
+    conn.close()
+    df['cosine_similarity'] = cosine_similarity(np.ones((1, len(tag_ids))),df.values).T
+    df.sort_values('cosine_similarity', inplace=True, ascending=False)
+    s = df[:top_n]['cosine_similarity']
+    return list(zip(s.index, s)), tag_ids
+
+
+@app.route('/poster')
+def picture():
+    return redirect('https://m.media-amazon.com/images/M/MV5BNTJjNmIzYWItNDE4OC00ZWQ3LWI0YzctODQyMTZjM2QxOWJhXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UY268_CR2,0,182,268_AL_.jpg')
 
 
 
--- visual/static/js/moreco.js
+++ visual/static/js/moreco.js
@@ -2,9 +2,100 @@ chosen_tags = []
 
 // console.log(window.location.pathname)
 
-var margin = {top: 50, right: 100, bottom: 20, left: 100}
-    , width = (window.innerWidth - margin.left - margin.right) / 2
-    , height = (window.innerHeight - margin.top - margin.bottom) / 2;
+var margin = {
+    top: 15,
+    right: 25,
+    bottom: 15,
+    left: 125
+};
+
+// hack: top 10 movies bar chart
+sample_movie = 
+    [{'id': 'kungfu1', 'relevance_score': '0.71'}, 
+     {'id': 'kungfu2', 'relevance_score': '0.72'}, 
+     {'id': 'kungfu3', 'relevance_score': '0.73'}, 
+     {'id': 'kungfu4', 'relevance_score': '0.74'}, 
+     {'id': 'kungfu5', 'relevance_score': '0.75'},
+     {'id': 'kungfu6', 'relevance_score': '0.76'},
+     {'id': 'kungfu7', 'relevance_score': '0.77'},
+     {'id': 'kungfu8', 'relevance_score': '0.78'},
+     {'id': 'kungfu9', 'relevance_score': '0.79'},
+     {'id': 'kungfu10', 'relevance_score': '0.80'}];
+
+var width = 700 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// Movie Relevance Chart
+
+var svg_barchart = d3.select("#barchart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr('class', 'barchart')
+    .style("visibility", "hidden");
+
+var tool_tip = d3.select("body").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
+
+var x_bar = d3.scaleLinear()
+    .range([0, width])
+    .domain([0, 1]);
+
+var y_bar = d3.scaleBand()
+    .range([height, 0], .5)
+    .domain(sample_movie.map(function (d) {
+        return d.id;
+    }));
+
+var yAxis_bar = d3.axisLeft(y_bar)
+    .tickSize(0)
+
+var xAxis_bar = d3.axisBottom()
+    .scale(x_bar).tickSize(5)
+
+svg_barchart.append("g")
+    .attr("class", "y_axis")
+    .call(yAxis_bar)
+
+svg_barchart.append("g")
+    .attr("class", "x_axis")
+    .call(xAxis_bar)
+    .selectAll("text")
+    .attr("transform", "translate(0,-20)" );
+
+var bars_bar = svg_barchart.selectAll(".bar")
+    .data(sample_movie)
+    .enter()
+    .append("g")
+
+bars_bar.append("rect")
+    .attr("class", "bar")
+    .attr("y", function (d) {
+        return y_bar(d.id);
+    })
+    .attr("height", y_bar.bandwidth())
+    .attr("x", 0)
+    .attr("width", function (d) {
+        return x_bar(d.relevance_score);
+    })
+    .style("fill", "blue");
+    
+
+
+// bars_bar.append("text")
+//     .attr("class", "label")
+//     .attr("y", function (d) {
+//         return y_bar(d.movie) + y_bar.bandwidth() / 2 + 4;
+//     })
+//     .attr("x", function (d) {
+//         return x_bar(d.tag_relevance) + 3;
+//     })
+//     .text(function (d) {
+//         return d.tag_relevance;
+//     });
+
 
 d3.dsv(",", "/genome-tags").then(function(data) {
     director_array = []
@@ -26,60 +117,6 @@ d3.dsv(",", "/genome-tags").then(function(data) {
     };
 
     $("#transfer1").transfer(settings);
-
-    var svg_barchart = d3.select("#barchart")
-        .append("svg")
-        .attr("width", 700)
-        .attr("height", 400)
-        .append("g")
-        .attr('class', 'barchart')
-        // .style("visibility", "hidden");
-    
-    var x_bar = d3.scaleLinear()
-        .range([0, width])
-        .domain([0, 1]);
-    
-    var y_bar = d3.scaleBand()
-        .range([height, 0], .1)
-        .domain([0, 1]);
-
-    var yAxis_bar = d3.axisLeft(y_bar)
-        .tickSize(0)
-    
-    var xAxis_bar = d3.axisBottom()
-        .scale(x_bar)
-
-    svg_barchart.append("g")
-        .attr("class", "y_axis")
-        .call(yAxis_bar)
-
-    svg_barchart.append("g")
-        .attr("class", "x_axis")
-        .call(xAxis_bar)
-
-    var bars_bar = svg_barchart.selectAll(".bar")
-        .data([])
-        .enter()
-        .append("g")
-
-    bars_bar.append("rect")
-        .attr("class", "bar")
-        .attr("y", function (d) {
-            return y_bar(d.key);
-        })
-        .attr("height", y_bar.bandwidth())
-        .attr("x", 0)
-        .attr("width", function (d) {
-            return x_bar(d.value);
-        })
-        .style("fill", "blue");
-            
-    bars_bar.append("text")
-        .attr("x", width/2)
-        .attr("y", height/2-200)
-        .style("text-anchor", "middle")
-        .attr("dy", ".35em")
-    
 });
 
 function send_tags_to_server() {
@@ -100,6 +137,70 @@ function send_tags_to_server() {
     });
 }
 
-function display_top_movies_barchart(top_movies) {
-    console.log("Displaying: " + top_movies)
+function display_top_movies_barchart(top_selection) {
+
+    top_selection.sort(function(x, y){
+        return d3.ascending(x.relevance_score, y.relevance_score)
+    })
+
+    y_bar.domain(
+        top_selection.map(function (d) {
+        return d.id;
+    }));
+
+    yAxis_bar = d3.axisLeft(y_bar)
+                .tickSize(3)
+
+    console.log(top_selection)
+    bars_bar.data(top_selection).enter().exit().remove()
+
+    bars_bar.select(".bar")
+        .attr("y", function (d) {
+            console.log(d.id)
+            return y_bar(d.id);
+        })
+        .attr("height", y_bar.bandwidth())
+        .attr("x", 0)
+        .attr("width", function (d) {
+            return x_bar(d.relevance_score);
+        })
+        .on("mouseover", function(d) {
+            d3.select(this).style("fill", "orange");
+
+            tool_tip.transition()		
+                .duration(200)		
+                .style("opacity", .9);		
+            
+            image_link = 'https://m.media-amazon.com/images/M/MV5BNTJjNmIzYWItNDE4OC00ZWQ3LWI0YzctODQyMTZjM2QxOWJhXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UY268_CR2,0,182,268_AL_.jpg'
+            tool_tip.html(
+                "<a href=imdb.com/title/" + d.id + ">" + d.id + "/</a>" + "<br/>" + "Relevance: " + d.relevance_score 
+                + "<br/>" + "<img src=" + image_link + ">")
+                
+                // "imdb.com/title/" + d.id + "<br/>"  + "Relevance: " + d.relevance_score 
+                //         + "<br/>" + "<img src=" + image_link + ">")	
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            d3.select(this).style("fill", "blue");
+            tool_tip.transition()		
+                .duration(500)		
+                .style("opacity", 0);
+        });
+
+    // bars_bar.select(".label")
+    //     .attr("y", function (d) {
+    //         return y_bar(d.movie) + y_bar.bandwidth() / 2 + 4;
+    //     })
+    //     .attr("x", function (d) {
+    //         return x_bar(d.tag_relevance) + 3;
+    //     })
+    //     .text(function (d) {
+    //         return d.tag_relevance;
+    //     });
+        
+
+    svg_barchart.select(".y_axis").call(yAxis_bar)
+    svg_barchart.style("visibility", "visible");
+
 }
